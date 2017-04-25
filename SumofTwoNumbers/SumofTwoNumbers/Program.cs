0192795d3e1c79992da9c212646af79917d67482
Christian <ccattell@corning-cc.edu>
2017-04-25 16:48:44 -0400

    Sum of two numbers console program

---
+++ SumofTwoNumbers/SumofTwoNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿//Collect two input numbers and calculate and print the sum
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumofTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("First number: ");
+            string i1 = Console.ReadLine();
+
+            System.Console.WriteLine("Second number: ");
+            string i2 = Console.ReadLine();
+
+            int n1, n2;
+            Int32.TryParse(i1, out n1);
+            Int32.TryParse(i2, out n2);
+            int sum = n1 + n2;
+
+            System.Console.WriteLine(n1 + " + " + n2 + " = " + sum);
+            Console.ReadKey();
+            
+        }
+    }
+}
