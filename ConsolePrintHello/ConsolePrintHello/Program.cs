b29e9371a6ade53e00977fa405b9557213abfd6c
Christian <ccattell@corning-cc.edu>
2017-04-25 16:23:13 -0400

    ConsolePrintHello

---
+++ ConsolePrintHello/ConsolePrintHello/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsolePrintHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("What is your name?");
+            System.Console.WriteLine("My name is: ");
+            string name = Console.ReadLine();
+
+            System.Console.WriteLine("Hello, " + name);
+            System.Console.ReadKey();
+
+        }
+    }
+}
